0a81f0a69b8749ddbd79d299f1cce05ef22ddc98
nord1690 <nord1690@mail.ru>
2017-06-09 20:11:50 +0300

    Update slicerDataLayer.js

--- slicerDataLayer.js
+++ slicerDataLayer.js
@@ -1,26 +1,101 @@
-slicer = function (){
-	window.dataLayer = window.dataLayer || []
+function Slicer (){
+
+	window.dataLayer = window.dataLayer || [] //Проверка dataLayer
 	var invalidEntries = 0;
-	var i2 = [];
+	var i2 = []; //номер dataLayer, где находится impress
 
-	function isObject(obj) {
-	  return obj!== undefined && typeof(obj) === 'object' && obj != null;
+	function isObject(obj) { //Функция проверки объекта
+
+		return obj!== undefined && obj!== null && typeof(obj) === 'object';
+
 	}
 
-	function filterByID(item, i) {
+	function filterByID(item, i) { //функция нахождения всех объектов с impress
 	  
-	  if (typeof(dataLayer) ==='object' && isObject(item.ecommerce)) {
-	  	i2.push(i)
-	    return true;
-	  } 
-	  invalidEntries++;
-	  return false; 
+		if ( 'ecommerce' in item && isObject(item.ecommerce) && isObject(item.ecommerce.impressions) ) {
+	  		
+	  		i2.push(i)
+
+	    	return true;
+	  	}
+
+	  	invalidEntries++;
+	  	return false; 
+	}
+	
+	var arrByID = dataLayer.filter(filterByID) // Фильтруем объекты с imress
+
+	function delImpressions (i2) { //Функция удаления impress
+
+		if(i2.length > 0){
+		
+			for ( var i = 0; i2.length > i; i++ ){
+
+				delete dataLayer[i2[i]]["ecommerce"].impressions
+			}
+
+			return;
+		}	
 	}
 
-	var arrByID = dataLayer.filter(filterByID)
+	function splitTo( arr, n) {
+ 		
+ 		var plen = Math.ceil(arr.length / n);
 
-	console.log(arrByID)
-	console.log(i2.length)
+  		return arr.reduce( function( p, c, i, a) {
+    
+	    	if(i%plen === 0) p.push({});
+	    	
+	    	p[p.length-1][i] = c;
+	    	
+	    	return p;
+	  		},
+  		[]);
+	}
 
-	return;
+	function startSlicer(i2) { // Старт работы функций
+
+		if ( arrByID.length > 0 ) {
+
+			for ( var j = 0; arrByID.length > j; j++ ) {
+
+				if ( arrByID[j].ecommerce.impressions.length < 16 ) {
+			
+					window.dataLayer.push({
+
+						event:"ShopEvent",
+						ecommerce : {
+							impressions: arrByID[j].ecommerce.impressions
+						}
+
+					})
+				} else {
+
+					if(isObject(arrByID[j].ecommerce.impressions)){
+
+						var arrSlice = []
+
+						arrSlice = splitTo( arrByID[j].ecommerce.impressions, Math.ceil(arrByID[j].ecommerce.impressions.length/15))
+
+						for ( var t = 0; arrSlice.length > t; t++ ) { 
+
+							window.dataLayer.push({
+
+								event:"ShopEvent",
+								ecommerce : {
+									impressions: arrSlice[t] 
+								}
+							})
+
+							console.log(arrSlice[t])
+						}
+					}	
+				}
+			}		
+
+			return delImpressions(i2);
+		}	
+	}
+	
+	return startSlicer(i2);
 }
